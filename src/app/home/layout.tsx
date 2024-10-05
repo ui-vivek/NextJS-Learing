d4f4f8dae57daf44b35fe91cc3a8d82d6b82d2d1
vivek singh <cu.vivek.1557@gmail.com>
2024-10-05 17:18:34 +0530

    conditional layout

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,14 +1,17 @@
+"use client"
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
 import './style.css'
 
 export default function RootLayout({ children }: any) {
+  const path = usePathname();
   return(
     <>
     <ul className="navBar">
         <li > <Link href='/home'>Home</Link></li>
         <li><Link href='/home/dashoard'>Dashoard</Link></li>
-        <li><Link href='/home/menu'>Manu</Link></li>
+        {path =='/home' ? <li><Link href='/home/menu'>Menu</Link></li> : null}
         <li><Link href='/home/about'>About</Link></li>
     </ul>
     <div>{children}</div>
