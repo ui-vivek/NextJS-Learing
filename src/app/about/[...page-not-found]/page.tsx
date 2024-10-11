6f2cb8cc664e12d81b9712c4e503175d5c14a4dc
vivek singh <cu.vivek.1557@gmail.com>
2024-10-11 15:33:20 +0530

    page not found

---
+++ src/app/about/[...page-not-found]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFond() {
+  return (
+    <div>
+      <h1>This About Page Not Found.</h1>
+      <p>This is the specific Page Not Found page for some route.</p>
+    </div>
+  )
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFond() {
+  return (
+    <div>
+      <h1>This page is not fond :( </h1>
+    </div>
+  )
+}
