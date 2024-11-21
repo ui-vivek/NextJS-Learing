3f3dca27dd75a63ad1c2d471ae3ae9dbd2925d4a
ui_vivek <cu.vivek.1557@gmail.com>
2024-11-21 17:20:59 +0530

    added loader

---
+++ public/testFunction.js
@@ -0,0 +1,4 @@
+function testFunction() {
+  return 50;
+}
+console.log("====[ test function loaded from public folder ]====");
--- src/app/item/page.tsx
+++ src/app/item/page.tsx
@@ -1,11 +1,24 @@
 "use client"
 
 import Link from "next/link";
+import Script from "next/script";
 
 const Item =()=>{
-   
+
     return(
         <div>
+            {/*
+            * Loads an external JavaScript file and logs a message upon successful loading.
+            * @param {string} src - The path to the JavaScript file to be loaded.
+            * @param {() => void} onLoad - A callback function that executes when the script is successfully loaded.
+            * @returns {JSX.Element} A Script component that loads the specified JavaScript file.
+            */}
+            <Script 
+            src="/testFunction.js"
+            onLoad={() => {
+                console.log("script loaded successfully")
+            }}
+            />
             <h1>This is Items Page</h1>
             <ul>
                 <li><Link href='/item/apple'>Apple</Link></li>
