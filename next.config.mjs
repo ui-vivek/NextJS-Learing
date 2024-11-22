daa857bea168e4166a39aa7cba67065fdadb7436
ui_vivek <cu.vivek.1557@gmail.com>
2024-11-22 18:27:00 +0530

    environment check-up

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+    basePath: process.env.NODE_ENV == "development" ? '/dev' : '/api',
     // output: 'export',
     /**
      * Set the output type for static file generation during npm run build.
